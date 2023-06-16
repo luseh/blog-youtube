f8440336fb80c3697912be72c6fb060b93f0257b
Isa Lucena <laluseh@gmail.com>
2023-06-16 18:23:45 +0200

    update_pkgs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 export default {
